75711ab83cbbdd6f70cf503ddc20d8ee6277bf3d
ssssbw <86484847+ssssbw@users.noreply.github.com>
2022-04-30 01:08:22 +0800

    first commit

---
+++ basis/merge_sort.cpp
@@ -0,0 +1,21 @@
+/*
+* @description: 归并排序模板
+* @author: sbw
+* @date: 2022-04-30 01:01:48
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e6 + 10;
+int arr[N], tem[N];
+
+void merge_sort(int *arr, int l, int r)
+{
+    if (l >= r) return;
+}
+
+int main()
+{
+    
+    return 0;
+}
